9c0617ea6ca3c485bc70670c04b39250b72ec914
Alden Bansemer <alldoneb@gmail.com>
2021-06-07 21:01:49 -0700

    Fixing Actions cache

--- src/main/java/com/knoban/atlas/missions/Missions.java
+++ src/main/java/com/knoban/atlas/missions/Missions.java
@@ -1,5 +1,7 @@
 package com.knoban.atlas.missions;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.*;
 
 public final class Missions {
@@ -9,24 +11,27 @@ public final class Missions {
     private final List<Class<? extends Mission>> missions = new ArrayList<>();
     private final Map<String, Class<? extends Mission>> missionsByName = new HashMap<>();
 
-    public void addMission(Class<? extends Mission> mission) {
+    public void addMission(@NotNull Class<? extends Mission> mission) {
         missions.add(mission);
         missionsByName.put(mission.getAnnotation(MissionInfo.class).name(), mission);
     }
 
-    public void removeMission(Class<? extends Mission> mission) {
+    public void removeMission(@NotNull Class<? extends Mission> mission) {
         missions.remove(mission);
         missionsByName.remove(mission.getAnnotation(MissionInfo.class).name());
     }
 
+    @NotNull
     public List<Class<? extends Mission>> getMissions() {
         return Collections.unmodifiableList(missions);
     }
 
+    @NotNull
     public Map<String, Class<? extends Mission>> getMissionByName() {
         return Collections.unmodifiableMap(missionsByName);
     }
 
+    @NotNull
     public static Missions getInstance() {
         return INSTANCE;
     }
